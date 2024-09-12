97869f0820b21dde828c71d45ff9b95d3257a08a
edson <edsonmar131@gmail.com>
2024-09-11 22:02:58 -0600

    Terminé la Tarea 5 de la Unidad 2

---
+++ main.py
@@ -0,0 +1,14 @@
+from circulo import Circulo
+
+circulo_uno = Circulo(3)
+circulo_dos = Circulo(10)
+circulo_tres = Circulo(20)
+
+circulo_uno.calcular_area()
+circulo_uno.calcular_perimetro()
+    
+circulo_dos.calcular_area()
+circulo_dos.calcular_perimetro()
+    
+circulo_tres.calcular_area()
+circulo_tres.calcular_perimetro()
